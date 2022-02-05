8f29452402295afffeb8071ef97c557a47eca124
Javiex <javiexalonso@gmail.com>
2022-02-06 00:24:01 +0100

    Basic cards logic UI completed
    Falta la mecánica de cpser Condiciones/Resultados

--- Assets/Scripts/CardDragger.cs
+++ Assets/Scripts/CardDragger.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class CardDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
@@ -6,22 +7,80 @@ public class CardDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
     public Transform returnParent = null;
     public bool playedCard;
 
+    public enum slotType
+    {
+        CARD,
+        CONDITION,
+        RESULT
+    }
+
+    public slotType typeOfSlot = slotType.CARD;
+
+    private Vector2 scale = new Vector2(1.4f, 1.4f);
+    private GameObject placeHolder = null;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
+        placeHolder = new GameObject();
+        placeHolder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeHolder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+
         returnParent = this.transform.parent;
         this.transform.SetParent(this.transform.parent.parent);
 
         GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+        this.transform.LeanScale(scale, 0.15f);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
+
+
+        int newPos = returnParent.GetChildCount();
+
+        for (int i = 0; i < returnParent.childCount; i++)
+        {
+            if (this.transform.position.x < returnParent.GetChild(i).position.x)
+            {
+                newPos = i;
+                if (placeHolder.transform.GetSiblingIndex() < newPos)
+                {
+                    newPos--;
+                }
+
+                break;
+            }
+
+            placeHolder.transform.SetSiblingIndex(newPos);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         this.transform.SetParent(returnParent);
+
+        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+        Destroy(placeHolder);
+
+
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        if (playedCard == true)
+        {
+            this.transform.LeanScale(new Vector2(1.3f, 1.3f), 0.15f);
+        }
+        else
+        {
+            this.transform.LeanScale(Vector2.one, 0.15f);
+        }
     }
 }
--- Assets/Scripts/DropCard.cs
+++ Assets/Scripts/DropCard.cs
@@ -3,18 +3,45 @@ using UnityEngine.EventSystems;
 
 public class DropCard : MonoBehaviour, IDropHandler
 {
-    private CardDragger actualCard = null;
+    private bool oneCard = false;
+    [SerializeField] private bool initialDeck = false;
+
+    public CardDragger.slotType typeOfSlot = CardDragger.slotType.CARD;
 
     public void OnDrop(PointerEventData eventData)
     {
         CardDragger cd = eventData.pointerDrag.GetComponent<CardDragger>();
-        if (cd != null && cd.playedCard == false)
+
+        if (typeOfSlot == cd.typeOfSlot)
         {
-            cd.returnParent = this.transform;
-            cd.playedCard = true;
+            if (cd.typeOfSlot == CardDragger.slotType.CARD)
+            {
+                // Check if the card has been played early if not, position the card in the slot
+                if (cd.playedCard == false && oneCard == false)
+                {
+                    if (initialDeck == false)
+                    {
+                        oneCard = true;
+                        cd.playedCard = true;
+                    }
+                    cd.returnParent = this.transform;
+                }
+                else if (cd.playedCard == true)
+                {
+                }
+            }
         }
-        else if (cd.playedCard == true)
+        else if (cd.typeOfSlot == CardDragger.slotType.CONDITION || cd.typeOfSlot == CardDragger.slotType.RESULT)
         {
+            // Check others types of cards (Conditions and Results)
+            if (oneCard == false)
+            {
+                if (initialDeck == false)
+                {
+                    oneCard = true;
+                }
+                cd.returnParent = this.transform;
+            }
         }
     }
 }
